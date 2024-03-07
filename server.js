660651255dab130e71333ce6aa6835c0fd58da23
annajantzen <aejantz2015@gmail.com>
2024-03-07 10:12:17 -0600

    added code to the server js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-const express = require('express');
-const PORT = 3001
+const express = require('express')
 const api = require ('./routes/index')
+const PORT = 3001
+
+const app = express()
+app.use(express.static('public'))
+app.use(api)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+})
